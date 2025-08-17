1fa22c75e30ad919769decdead72551904f93731
JohirU-coder <112777589+JohirU-coder@users.noreply.github.com>
2025-08-17 01:29:54 -0400

    fixed cors issues

--- rentreviews-platform/rentreviews-platform/rentreviews-platform/backend/auth-service/server.js
+++ rentreviews-platform/rentreviews-platform/rentreviews-platform/backend/auth-service/server.js
@@ -92,8 +92,10 @@ app.use(cors({
     const allowedOrigins = [
       'http://localhost:5500',
       'http://127.0.0.1:5500',
-      'http://localhost:3000',
-      'http://127.0.0.1:3000'
+      'http://localhost:3000', 
+      'http://127.0.0.1:3000',
+      'http://localhost:8080',
+      'http://127.0.0.1:8080'
     ];
     
     if (allowedOrigins.indexOf(origin) !== -1) {
